112b1e94b6f756f650f4394264994543c8c5ca6f
symiac <EC_Base@symiac>
2019-08-26 09:56:28 +0800

    main.js中添加element组件swiper.vue中引入

--- fontend/src/components/swiper/swiper.vue
+++ fontend/src/components/swiper/swiper.vue
@@ -1,17 +1,25 @@
 <template>
     <div>
+        
+        
         <img src='@/assets/xigua.jpg'>
-        I am swiper
+        
     </div>
 </template>
 
 <script>
+import "https://unpkg.com/element-ui/lib/index.js"
+import "https://unpkg.com/element-ui/lib/theme-chalk/index.css"
     export default {
-        name:"v-swiper"
+        name:"v-swiper",
+        components:{
+      
+        }
     }
 </script>
 
 <style scoped>
+
 img{
     width: 100%;
     height: 700px;
