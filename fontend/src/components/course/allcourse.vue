88e15657bd5188da707182b742501c547cfb5b75
symiac <EC_Base@symiac>
2019-09-06 13:02:16 +0800

    tan

---
+++ fontend/src/components/course/allcourse.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="allcourse">
+     <header></header>
+      <div class="c-header" >
+            <h1>全部课程</h1>
+        </div>
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+ data() {
+ return {
+
+ }
+ },
+ components: {
+
+ }
+}
+</script>
+
+<style scoped>
+</style>
--- fontend/src/components/course/course.vue
+++ fontend/src/components/course/course.vue
@@ -1,4 +1,5 @@
 <template>
+
  <div class="course" id="course">
      <div class="c-header" >
             <h1>课程体系</h1>
@@ -9,13 +10,32 @@
                <div class="cc">
                 <div class="image">
                   <div class="image-bg">
-                    <img src="@/assets/shu.jpg" alt="Free Bootstrap Template by sc.chinaz.com">
+                    <img src="@/assets/shu.jpg" alt="课程图片（书本）">
                   </div>
                 </div>
                 <div class="text">
                   <h3>课程名字</h3>
                   <p>描述Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam quos quis nisi voluptatum architecto rerum error。</p>
-                   <p><button class="btn btn-primary">more information</button> </p>
+                   
+                      <div class="mask" v-if="showModal" @click="showModal=false"></div>   
+                        <div class="pop" v-if="showModal">
+                     <button @click="showModal=false" class="btn btn-danger btn-xs" id="close">
+                       <i class="fa fa-times-circle" aria-hidden="true"></i>
+                       </button>
+
+                       <div class="morecontent">
+                         <div class="left">
+                         <h3>课程名称</h3>
+                          <span> 这是教师详情</span>
+                         </div>
+                         <div class="right">
+                           <img src="">
+                         </div>
+                       </div>
+                     </div>
+
+                   <button @click="showModal=true" class="btn btn-primary">more information</button>
+                   
                 </div>
                 
               </div>
@@ -70,7 +90,7 @@
           </div>
            <div class="more"> 
          <div class="icon">
-                    <a href="#">      <i class="fa fa-chevron-circle-down" aria-hidden="true">更多</i></a>
+                    <a href="./allcourse">      <i class="fa fa-chevron-circle-down" aria-hidden="true">更多</i></a>
                         </div>
           </div>
  </div>
@@ -80,11 +100,14 @@
 export default {
  data() {
  return {
-
+      showModal: false
  }
  },
  components: {
 
+ },
+ methods: {
+  
  }
  
 }
@@ -165,5 +188,39 @@ a:link,a:hover,a:active,a:visited{
   opacity: 0.6;
  /* transform: scale(0.9); */
  /* transition: all 0.5s ease;*/
+}
+.btn:hover{
+
+   transform:scale(1.1);
+  transition: all 0.3s ease;
+}
+
+.mask {
+  background-color: #666a73;
+  opacity: 0.5;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1
+}
+.pop {
+  background-color: rgba(208, 209, 211, 0.658);
+ 
+  position: fixed;
+  top: 100px;
+  left: 300px;
+  width: calc(100% - 600px);
+  height:calc(100% - 200px);
+  z-index: 2;
+ 
+ 
+}
+#close{
+  float: right;
+}
+.morecontent{
+  margin-top: 20px;
 }
 </style>
